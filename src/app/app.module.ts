a7bbc564818a1b73405dc2dcf09c1b82006818b3
MO7S3N <34940993+MO7S3N@users.noreply.github.com>
2020-08-23 02:32:49 +0100

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { DadComponent } from './dad/dad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    DadComponent
   ],
   imports: [
     BrowserModule,
